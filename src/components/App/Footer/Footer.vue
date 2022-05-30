042cf92aaabbbbc5c2bcb092e632852f729c712f
Francis Berger <francisberger@MacBook-Pro-de-Napoleon.local>
2022-05-31 00:50:51 +0200

    deploy: new version

--- src/components/App/Footer/Footer.vue
+++ src/components/App/Footer/Footer.vue
@@ -1,7 +1,6 @@
 <template>
 	<footer class="fixed bottom-0 w-full bg-transparent pt-2 pb-3 px-8 flex justify-end wsc-text-default">
 		<div class="flex gap-2 group py-2 text-[#28286e]">
-			<p class="text-xs font-ptmono font-bold">IPFS hosted |</p>
 			<div class="flex items-center gap-2">
 				<uil:twitter class="w-4 h-4" />
 				<a href="https://twitter.com/fran6brg" rel="nofollow noopener" target="_blank">
@@ -11,6 +10,7 @@
 					<p class="flex items-center text-xs">@Mersona5</p>
 				</a>
 			</div>
+			<p class="text-xs font-ptmono font-bold">| IPFS hosted</p>
 		</div>
 	</footer>
 </template>
